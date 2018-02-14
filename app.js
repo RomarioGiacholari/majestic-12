38bfbe2490bd7d0d4f4ee0a804d66e1fab1442ea
RomarioGiacholari <giacholari@gmail.com>
2018-02-14 01:25:52 +0000

    set values to the pie chart

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var data = [{
-    values: [19, 26, 55,5,6,8,4,4],
+    values: [4.6, 1.725, 1.725, 1.15, 1.15, 0.46, 0.345, 0.23, 0.115 ],
     labels: ['Canada', 'USA', 'New Zealand', 'UK', 'Germany', 'France', 'India','China', 'Mozambique'],
     type: 'pie'
   }];
